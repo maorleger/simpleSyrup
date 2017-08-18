bead534e4b4c4e14bdba254e6205800227a119fd
Maor Leger <maor.leger@gmail.com>
2017-08-18 14:31:37 -0600

    [#150373204] setup Credo

--- lib/simpleSyrup/application.ex
+++ lib/simpleSyrup/application.ex
@@ -1,5 +1,8 @@
 defmodule SimpleSyrup.Application do
+  @moduledoc false
   use Application
+
+  alias SimpleSyrupWeb.Endpoint
 
   # See https://hexdocs.pm/elixir/Application.html
   # for more information on OTP Applications
@@ -25,7 +28,7 @@ defmodule SimpleSyrup.Application do
   # Tell Phoenix to update the endpoint configuration
   # whenever the application is updated.
   def config_change(changed, _new, removed) do
-    SimpleSyrupWeb.Endpoint.config_change(changed, removed)
+    Endpoint.config_change(changed, removed)
     :ok
   end
 end
