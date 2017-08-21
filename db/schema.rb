e084055a221e24d71f943bce6e27650081c42b3d
Maor Leger <maor.leger@gmail.com>
2017-08-21 11:07:18 -0600

    [#150388242] add location attributes to event

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170821162250) do
+ActiveRecord::Schema.define(version: 20170821170509) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 20170821162250) do
     t.integer "num_days"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.decimal "lat", precision: 10, scale: 6
+    t.decimal "lng", precision: 10, scale: 6
   end
 
   create_table "expenses", force: :cascade do |t|
