8e28980782c4b499cecef7707ee0b673e7221d4a
Maor Leger <maor.leger@gmail.com>
2017-08-21 10:17:48 -0600

    [#150412066] participants should have status

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -1,4 +1,5 @@
 class Participant < ApplicationRecord
+  enum status: [ :invited, :tentative, :accepted, :declined ]
   belongs_to :user
   belongs_to :event
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170820171721) do
+ActiveRecord::Schema.define(version: 20170821161033) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20170820171721) do
     t.bigint "event_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "status", default: 0
     t.index ["event_id"], name: "index_participants_on_event_id"
     t.index ["user_id"], name: "index_participants_on_user_id"
   end
