bce55a89b78de27909909b2cbc7fdeb5efc07dbf
Maor Leger <maor.leger@gmail.com>
2017-09-10 15:46:58 -0600

    [#150951931] omniauth with rails and devise - only starting point

--- rails/app/models/user.rb
+++ rails/app/models/user.rb
@@ -2,8 +2,25 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable,
+         omniauth_providers: [:google_oauth2]
 
   has_many :participants
   has_many :events, through: :participants
+
+  def self.from_omniauth(omniauth_data)
+    data = omniauth_data.info
+    user = User.find_by_email(data['email'])
+    # todo: getting password cant be blank here. I need to somehow suppress it
+
+    puts data.inspect
+    unless user
+      user = User.create!(
+        first_name: data['given_name'],
+        last_name: data['family_name'],
+        email: data['email'],
+      )
+    end
+    user
+  end
 end
