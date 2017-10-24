c11cc9b7b14fa202d492113ca01a876101dc76db
Maor Leger <maor.leger@gmail.com>
2017-10-24 16:27:41 -0600

    Remove the angular controller until we need it

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,5 @@
 Rails.application.routes.draw do
   post "/graphql", to: "graphql#execute"
   resources :events
-  resources :angular, only: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
