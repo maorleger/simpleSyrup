f0b80af2cb82bc424d76ba266c49a5a0ac335291
Maor Leger <maor.leger@gmail.com>
2017-10-25 17:35:52 -0600

    cleanup some comments

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -36,12 +36,10 @@ class EventsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_event
       @event = Event.find(params[:id])
     end
 
-    # Only allow a trusted parameter "white list" through.
     def event_params
       params.require(:event).permit(:name, :description, :start_date, :num_days, :lat, :lng)
     end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,4 @@
 Rails.application.routes.draw do
   post "/graphql", to: "graphql#execute"
   resources :events
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
