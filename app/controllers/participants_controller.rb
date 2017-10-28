d8466d1212a428a57e5290dc9274da2e17c2ebf0
Maor Leger <maor.leger@gmail.com>
2017-10-28 14:21:34 -0600

    Tests for bad cases

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -5,16 +5,12 @@ class ParticipantsController < ApplicationController
     @participant = Participant.new(participant_params).tap do |p|
       p.save
     end
-    # todo:
-    # 1. figure out the location bit from event controller
-    # 2. tests and implementation for bad case (event id is nil)
     render json: @participant, status: :created
-    # Participant.create(participant_params)
   end
 
   private
 
     def participant_params
-      params.permit(:event_id, :user_id)
+      params.permit(:event_id, :user_id, :status)
     end
 end
--- spec/controllers/participants_controller_spec.rb
+++ spec/controllers/participants_controller_spec.rb
@@ -9,24 +9,41 @@ RSpec.describe ParticipantsController, type: :controller do
     let(:post_params) do
       {
         event_id: event.id,
-        user_id: user.id
+        user_id: user.id,
+        status: "invited"
       }
-    end
 
-    it "returns http success" do
-      post :create, params: post_params
-      expect(response).to have_http_status(:success)
     end
-
-    it "creates a new participant" do
-      expect {
+    describe "when the participant is valid" do
+      it "returns http success" do
         post :create, params: post_params
-      }.to change { Participant.count }.by(1)
+        expect(response).to have_http_status(:success)
+      end
 
-      participant = Participant.last
-      expect(participant.event).to eq(event)
-      expect(participant.user).to eq(user)
-      expect(response.body).to eq(participant.to_json)
+      it "creates a new participant" do
+        expect {
+          post :create, params: post_params
+        }.to change { Participant.count }.by(1)
+
+        participant = Participant.last
+        expect(participant.event).to eq(event)
+        expect(participant.user).to eq(user)
+        expect(response.body).to eq(participant.to_json)
+      end
+    end
+
+    describe "when the participant is invalid" do
+      it "because it has no event id" do
+        expect do
+          post :create, params: post_params.except(:event_id)
+        end.to raise_error(ActionController::UrlGenerationError)
+      end
+
+      it "because it has in invalid status" do
+        expect do
+          post :create, params: post_params.merge(status: "foobar")
+        end.to raise_error(ArgumentError)
+      end
     end
   end
 end
