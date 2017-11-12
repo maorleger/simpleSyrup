c4e016bb34b6a4a2bbe5d8af8438113242f41195
Maor Leger <maor.leger@gmail.com>
2017-11-12 09:11:05 -0700

    API should camelCase data

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe UsersController, type: :controller do
     it "returns all users as json" do
       get :index
       expect(response).to have_http_status(:success)
-      expect(json.map { |j| j["first_name"] }).to eq(users.map(&:first_name))
+      expect(json.map { |j| j["firstName"] }).to eq(users.map(&:first_name))
     end
   end
 
@@ -21,8 +21,8 @@ RSpec.describe UsersController, type: :controller do
       it "returns the user as json" do
         get :show, params: { id: user.to_param }
         expect(response).to have_http_status(:success)
-        expect(json["first_name"]).to eq(user.first_name)
-        expect(json["last_name"]).to eq(user.last_name)
+        expect(json["firstName"]).to eq(user.first_name)
+        expect(json["lastName"]).to eq(user.last_name)
         expect(json["email"]).to eq(user.email)
         expect(json["id"]).to eq(user.id)
       end
