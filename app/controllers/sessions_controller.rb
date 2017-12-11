890553c3bcf448edd3fe86728a1ab4df55158e34
Maor Leger <maor.leger@gmail.com>
2017-12-10 19:14:52 -0600

    working on sessions controller spec

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
 
 class SessionsController < ApplicationController
+  def create
+    @user = User.find_or_create_from_auth(auth)
+    session[:user_id] = @user.id
+    render json: {}
+  end
+
+  private
+
+    def auth
+      GoogleAuth.new(request.env["omniauth.auth"])
+    end
 end
