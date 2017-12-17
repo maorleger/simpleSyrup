a3b5a0b38dfecb9a578003a0027cf47e9f9d81d8
Maor Leger <maor.leger@gmail.com>
2017-12-17 10:58:41 -0700

    guard against logged out users and add login page

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -3,18 +3,30 @@
 require "rails_helper"
 
 RSpec.describe HomeController, type: :controller do
-  let(:valid_session) { {} }
-
   describe "GET #show" do
     let(:subject) { get :show }
     let(:event) { create(:event) }
 
-    it "succeeds" do
-      # expect(subject).to have_http_status(200)
+    describe "when a user is not logged in" do
+      it "redirects to login path" do
+        expect(subject).to redirect_to(sessions_path)
+      end
     end
 
-    it "renders the public index file" do
-      # expect(subject).to render_template(file: "index.html")
+    describe "with a logged in user" do
+      let(:user) { create(:user) }
+
+      before(:each) do
+        session[:user_id] = user.id
+      end
+
+      it "succeeds" do
+        expect(subject).to have_http_status(200)
+      end
+
+      it "renders the public index file" do
+        expect(subject).to render_template(file: "index.html")
+      end
     end
   end
 end
