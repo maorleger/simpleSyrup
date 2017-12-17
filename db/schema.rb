0be24cdec422bbde72cb4320cc1609a68568bb1e
Maor Leger <maor.leger@gmail.com>
2017-12-17 10:28:16 -0700

    Sessions controller is done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171208014724) do
+ActiveRecord::Schema.define(version: 20171217162112) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,8 +43,8 @@ ActiveRecord::Schema.define(version: 20171208014724) do
     t.string "photo_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "provider"
     t.string "uid"
+    t.string "provider"
   end
 
   add_foreign_key "participants", "events"
